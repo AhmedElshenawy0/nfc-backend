ecba5ed394b1e75118ed132385e88eaff3fa7743
AhmedElshenawy0 <shenawy100@gmail.com>
2025-04-27 15:14:38 +0300

    deploy

--- src/server.ts
+++ src/server.ts
@@ -60,14 +60,14 @@ app.use("/api/services", serviceRoute);
 app.use("/api/soldServices", soldServiceRoute);
 
 // Serve uploaded files statically
-// app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(errorHandler as express.ErrorRequestHandler);
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../public/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 //=> Start server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
