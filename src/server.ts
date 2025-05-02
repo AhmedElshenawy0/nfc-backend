49e7f2a6f3d55de9bdb342c0a09517fe703f143e
AhmedElshenawy0 <shenawy100@gmail.com>
2025-05-02 11:08:42 +0300

    deploy

--- src/server.ts
+++ src/server.ts
@@ -17,8 +17,6 @@ import bodyParser from "body-parser";
 import "./utils/passport";
 import path from "path";
 import { errorHandler } from "./middleware/ErrorHandler";
-import fs from "fs";
-
 dotenv.config();
 
 const app = express();
@@ -87,10 +85,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-const uploadDir = path.join(__dirname, "..", "uploads");
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
 //=> Start server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
